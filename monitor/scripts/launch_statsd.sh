11760657a641911433ae5ec6819bb34ac7270355
Mayank Patel <mayank@side.cr>
2015-01-29 11:27:41 -0800

    Added statsd configuration and forwarding for librato.

---
+++ monitor/scripts/launch_statsd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USER=ubuntu
+GROUP=ubuntu
+PUBNUB_MON_ROOT=/home/ubuntu/PubnubTest
+STATSD_ROOT=/home/ubuntu/statsd
+CONFIG_FILE=$PUBNUB_MON_ROOT/monitor/config/statsd/statsd-config.js
+LOG_DIR=/var/log/statsd
+LOG_FILE=$LOG_DIR/statsd.log
+
+/usr/bin/sudo mkdir -p $LOG_DIR
+sudo chown $USER:$GOUP $LOG_DIR
+
+cd $STATSD_ROOT
+
+npm install > /dev/null 2>&1
+
+exec nodejs stats.js $CONFIG_FILE >> $LOG_FILE 2>&1
